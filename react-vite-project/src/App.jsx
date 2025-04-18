e728ebb3357f4bd6eeb73db6d997a8947d6d271e
Aram Margaryan <hrayryan77@gmail.com>
2025-04-18 15:26:59 +0300

    feat: Implemented logic for input and item list: - Prompt for new value on "Enter new" button click - Validation: minimum 3 characters - Show error for invalid input - Display current value under the heading - Add value to the list on "Add to list" click - Clear current value after adding - Disable "Add to list" button if value is invalid - Show "No items added" message if the list is empty

---
+++ react-vite-project/src/App.jsx
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+import styles from './app.module.css';
+
+const isValid = (str) => {
+	return typeof str === 'string' && str.trim().length >= 3;
+};
+
+export const App = () => {
+	const [value, setValue] = useState('');
+	const [list, setList] = useState([]);
+	const [error, setError] = useState('');
+
+	const isValueValid = isValid(value);
+
+	const onInputButtonClick = () => {
+		const promptValue = prompt('Set value');
+
+		if (!isValid(promptValue)) {
+			setValue(promptValue);
+			setError('Введенное значение должно содержать минимум 3 символа');
+			return;
+		}
+
+		error.length > 0 && setError('');
+		setValue(promptValue);
+	};
+
+	const getFormattedDateTime = () => {
+		const now = new Date();
+
+		const pad = (n) => n.toString().padStart(2, '0');
+
+		const day = pad(now.getDate());
+		const month = pad(now.getMonth() + 1);
+		const year = now.getFullYear();
+
+		const hours = pad(now.getHours());
+		const minutes = pad(now.getMinutes());
+		const seconds = pad(now.getSeconds());
+
+		return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+	};
+
+	const onAddButtonClick = () => {
+		setValue('');
+		const formattedDate = getFormattedDateTime();
+		const updatedList = [
+			...list,
+			{
+				id: ++list.length,
+				value: value,
+				createdAt: formattedDate,
+			},
+		];
+		setList(updatedList);
+	};
+
+	return (
+		<div className={styles.app}>
+			<h1 className={styles.pageHeading}>Ввод значения</h1>
+			<p className={styles.noMarginText}>
+				Текущее значение <code>value</code>: {'"'}
+				<output className={styles.currentValue}>{value}</output>
+				{'"'}
+			</p>
+			{error !== '' && <div className={styles.error}>{error}</div>}
+			<div className={styles.buttonsContainer}>
+				<button className={styles.button} onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
+				<button
+					className={styles.button}
+					disabled={!isValueValid}
+					onClick={onAddButtonClick}
+				>
+					Добавить в список
+				</button>
+			</div>
+			<div className={styles.listContainer}>
+				<h2 className={styles.listHeading}>Список:</h2>
+
+				{list.length === 0 ? (
+					<p className={styles.noMarginText}>Нет добавленных элементов</p>
+				) : (
+					<ul className={styles.list}>
+						{list.map(({ id, value, createdAt }) => {
+							console.log('createdAt', createdAt);
+							return (
+								<li key={id} className={styles.listItem}>
+									{value}
+								</li>
+							);
+						})}
+					</ul>
+				)}
+			</div>
+		</div>
+	);
+};
